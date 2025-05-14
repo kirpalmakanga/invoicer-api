3ea3406f5b6dd327181f7acd0ad0f5de243df633
Kirpal Makanga <makanga.kirpal@gmail.com>
2025-05-14 22:30:16 +0200

    CustomerResource: selected relevant data

--- app/Http/Resources/CustomerResource.php
+++ app/Http/Resources/CustomerResource.php
@@ -14,6 +14,12 @@ class CustomerResource extends JsonResource
      */
     public function toArray(Request $request): array
     {
-        return parent::toArray($request);
+        return [
+            'id' => $this->id,
+            'name' => $this->name,
+            'address' => $this->address,
+            'tel' => $this->tel,
+            'email' => $this->email,
+        ];
     }
 }

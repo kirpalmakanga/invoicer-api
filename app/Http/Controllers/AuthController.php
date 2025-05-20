e0d7e5a1bafc18ec7ebf85e35c74a5e0cb1836b3
Kirpal Makanga <makanga.kirpal@gmail.com>
2025-05-20 18:40:57 +0200

    AuthController: removed unused import

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -8,7 +8,6 @@ use Illuminate\Support\Facades\Validator;
 use Illuminate\Http\Request;
 use App\Models\User;
 use Illuminate\Support\Facades\Http;
-use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Session;
 use Illuminate\Support\Str;
 use Laravel\Passport\Token;
